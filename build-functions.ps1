5eba56e745d2d9cdcae69b975afd687b359828b3
Pavan <jpavan2006@gmail.com>
2021-08-03 19:35:58 -0700

    proc call from tc

--- build-functions.ps1
+++ build-functions.ps1
@@ -33,3 +33,90 @@ $workspace
 	}
 
 }
+
+function ExecuteQuery {
+
+[CmdletBinding()]
+[OutputType([void])]
+param
+(
+[Parameter(Mandatory)]
+$instance,
+[Parameter(Mandatory)]
+$dbname,
+[Parameter(Mandatory)]
+$query
+)
+try {
+
+  $connectionString="server=$instance;initial catalog=$dbname;integrated security=sspi;Connection timeout=3600"
+  $sqlquery="$query"
+  $sqlconnection= New-Object System.Data.SqlClient.SqlConnection
+  $sqlconnection.ConnectionString=$connectionString
+  $sqlcmd=New-Object  System.Data.SqlClient.SqlCommand
+  $sqlcmd.CommandText=$sqlQuery
+  $sqlcmd.Connection=$sqlconnection
+  
+  $sqladaptor= New-Object  System.Data.SqlClient.SqlDataAdapter
+  $sqladaptor.SelectCommand=$sqlcmd
+  $ResultSet= New-Object System.Data.DataSet
+  $recordcount=$sqladapter.Fill($ResultSet)
+  
+  $sqlconnection.close()  
+
+  $data =$ResultSet.Tables[0]
+  
+  return $data
+}catch{
+
+Write-Error "Query execution failed : $_.Exception.Message" -ErrorAction Stop
+Exit 1
+break
+}
+finally{
+ $SqlCmd.Dispose()
+ $SqlConnection.Dispose()
+}
+}
+
+function ExecuteNonQuery {
+
+[CmdletBinding()]
+[OutputType([void])]
+param
+(
+[Parameter(Mandatory)]
+$instance,
+[Parameter(Mandatory)]
+$dbname,
+[Parameter(Mandatory)]
+$query
+)
+try {
+
+  $connectionString="server=$instance;initial catalog=$dbname;integrated security=sspi;Connection timeout=3600"
+  $sqlquery="$query"
+  $sqlconnection= New-Object System.Data.SqlClient.SqlConnection
+  $sqlconnection.ConnectionString=$connectionString
+  $sqlconnection.Open()
+  $sqlcmd=$sqlconnection.CreateCommand()
+  $sqlcmd.CommandText=$sqlquery
+  $sqlcmd.CommandTimeout=0
+  $sqlcmd.ExecuteNonQuery()
+  $sqlconnection.close()  
+}catch{
+
+Write-Error "NonQuery execution failed : $_.Exception.Message" -ErrorAction Stop
+Exit 1
+break
+}
+finally{
+ $SqlCmd.Dispose()
+ $SqlConnection.Dispose()
+}
+}
+
+
+
+
+#
